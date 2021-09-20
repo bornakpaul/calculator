3064eb703efa285ae287939898ba36e1a4fe09dc
Bornak <bornakpaul@gmail.com>
2021-09-20 10:42:08 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function insert(num){
+    document.getElementById("textview").value = document.getElementById("textview").value + num;
+}
+
+function clean(){
+    document.getElementById("textview").value="";
+}
+
+function calc(){
+    
+}
