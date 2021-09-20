4971ee42b44e6b83b6981e1141bfdd1c91a2324d
Bornak <bornakpaul@gmail.com>
2021-09-20 14:16:13 +0530

    new

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ function clean(){
     document.getElementById("textview").value="";
 }
 
+function calc(){
+    temp = document.getElementById("textview").value;
+    result = eval(temp);
+    document.getElementById("textview").value=result;
+}
+
+function back(){
+    temp = document.getElementById("textview").value;
+    document.getElementById("textview").value = temp.substring(0, temp.length-1);
+}
