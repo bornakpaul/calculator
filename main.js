23757f300a15f11a9ba2c6fa43993162cabeaf4f
Bornak <bornakpaul@gmail.com>
2021-09-20 11:43:46 +0530

    no changes

--- main.js
+++ main.js
@@ -6,13 +6,3 @@ function clean(){
     document.getElementById("textview").value="";
 }
 
-function calc(){
-    temp = document.getElementById("textview").value;
-    result = eval(temp);
-    document.getElementById("textview").value=result;
-}
-
-function back(){
-    temp = document.getElementById("textview").value;
-    document.getElementById("textview").value = temp.substring(0, temp.length-1);
-}
